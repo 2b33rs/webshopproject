00e012b5e793c605815aaf5744537e051ba96f79
Tobias <84900385+2b33rs@users.noreply.github.com>
2023-05-31 11:26:44 +0200

    add prices

--- database/insert.sql
+++ database/insert.sql
@@ -40,11 +40,11 @@ INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `descr
 VALUES (3, 1, 'Microsoft OneDrive', 'Cloud-Speicher von Microsoft', 'Kostenlos');
 
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
-VALUES (4, 2, 'Google One', 'Erweiterter Cloud-Speicher von Google', 'Preis abhängig von Speicher');
+VALUES (4, 2, 'Google One', 'Erweiterter Cloud-Speicher von Google', '3.99');
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
-VALUES (5, 2, 'Dropbox Plus', 'Erweiterte Funktionen für Dropbox', '9.99 EUR/Monat');
+VALUES (5, 2, 'Dropbox Plus', 'Erweiterte Funktionen für Dropbox', '9.99');
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
-VALUES (6, 2, 'Microsoft 365', 'Umfassendes Produktivitätspaket', 'Preis abhängig vom Plan');
+VALUES (6, 2, 'Microsoft 365', 'Umfassendes Produktivitätspaket', '12.39');
 
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
 VALUES (7, 3, 'Adobe Creative Cloud', 'Cloud-Speicher für Kreative', 'Preis abhängig von Plan');
@@ -54,45 +54,45 @@ INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `descr
 VALUES (9, 3, 'Vimeo', 'Videohosting und Cloud-Speicher', 'Preis abhängig von Plan');
 
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
-VALUES (10, 4, 'Google One', 'Erweiterter Cloud-Speicher von Google', 'Preis abhängig von Speicher');
+VALUES (10, 4, 'Google One', 'Erweiterter Cloud-Speicher von Google', '3.99');
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
-VALUES (11, 4, 'Dropbox Plus', 'Erweiterte Funktionen für Dropbox', '9.99 EUR/Monat');
+VALUES (11, 4, 'Dropbox Plus', 'Erweiterte Funktionen für Dropbox', '9.99');
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
-VALUES (12, 4, 'Microsoft 365', 'Umfassendes Produktivitätspaket', 'Preis abhängig vom Plan');
+VALUES (12, 4, 'Microsoft 365', 'Umfassendes Produktivitätspaket', '123.12');
 
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
 VALUES (13, 5, 'iCloud', 'Cloud-Speicher von Apple', 'Preis abhängig vom Speicherplan');
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
-VALUES (14, 5, 'Box', 'Sicherer Cloud-Speicher', 'Preis abhängig vom Plan');
+VALUES (14, 5, 'Box', 'Sicherer Cloud-Speicher', '123.12');
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
 VALUES (15, 5, 'Amazon Drive', 'Cloud-Speicher von Amazon', 'Preis abhängig vom Speicherplan');
 
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
-VALUES (16, 6, 'Google Workspace', 'Kollaborations- und Produktivitätstools', 'Preis abhängig vom Plan');
+VALUES (16, 6, 'Google Workspace', 'Kollaborations- und Produktivitätstools', '1312.12');
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
-VALUES (17, 6, 'Dropbox Business', 'Cloud-Speicher und Kollaboration für Unternehmen', 'Preis abhängig vom Plan');
+VALUES (17, 6, 'Dropbox Business', 'Cloud-Speicher und Kollaboration für Unternehmen', '499.99');
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
-VALUES (18, 6, 'Microsoft 365 Business', 'Umfassende Lösung für Unternehmen', 'Preis abhängig vom Plan');
+VALUES (18, 6, 'Microsoft 365 Business', 'Umfassende Lösung für Unternehmen', '39.99');
 
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
-VALUES (19, 7, 'Adobe Creative Cloud', 'Kreative Tools für Fotografen', 'Preis abhängig vom Plan');
+VALUES (19, 7, 'Adobe Creative Cloud', 'Kreative Tools für Fotografen', '19.99');
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
-VALUES (20, 7, 'SmugMug', 'Plattform für Fotografen zur Präsentation ihrer Werke', 'Preis abhängig vom Plan');
+VALUES (20, 7, 'SmugMug', 'Plattform für Fotografen zur Präsentation ihrer Werke', '13.99');
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
-VALUES (21, 7, '500px', 'Online-Community für Fotografen', 'Preis abhängig vom Plan');
+VALUES (21, 7, '500px', 'Online-Community für Fotografen', '12.99');
 
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
-VALUES (22, 8, 'SoundCloud', 'Plattform für Musiker zum Teilen und Entdecken von Musik', 'Preis abhängig vom Plan');
+VALUES (22, 8, 'SoundCloud', 'Plattform für Musiker zum Teilen und Entdecken von Musik', '12.88');
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
-VALUES (23, 8, 'Bandcamp', 'Unabhängige Musikplattform für Künstler', 'Preis abhängig vom Plan');
+VALUES (23, 8, 'Bandcamp', 'Unabhängige Musikplattform für Künstler', '1337.88');
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
 VALUES (24, 8, 'Spotify for Artists', 'Tools für Musiker zur Verwaltung und Promotion ihrer Musik', 'Kostenlos');
 
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
-VALUES (25, 9, 'Vimeo', 'Plattform für Video-Hosting und -Sharing', 'Preis abhängig vom Plan');
+VALUES (25, 9, 'Vimeo', 'Plattform für Video-Hosting und -Sharing', '1337.88');
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
-VALUES (26, 9, 'Wistia', 'Video-Hosting-Plattform für Unternehmen', 'Preis abhängig vom Plan');
+VALUES (26, 9, 'Wistia', 'Video-Hosting-Plattform für Unternehmen', '123.45');
 INSERT INTO `webshop`.`product` (`product_id`, `subcategorie_id`, `name`, `description`, `price`) 
-VALUES (27, 9, 'Brightcove', 'Video-Plattform für Unternehmen und Content-Ersteller', 'Preis abhängig vom Plan');
+VALUES (27, 9, 'Brightcove', 'Video-Plattform für Unternehmen und Content-Ersteller', '12.39');
 
 
