ca4c2056df5873477679c2697582417e60f41b27
Tobias <84900385+2b33rs@users.noreply.github.com>
2023-06-01 17:17:07 +0200

    zwischenstasnd

--- _public/php/paypal_integration_php/success.php
+++ _public/php/paypal_integration_php/success.php
@@ -1,56 +1,69 @@
-<?php 
+<?php
 // Include configuration file 
-include_once 'config.php'; 
- 
-// Include database connection file 
-include_once 'dbConnect.php'; 
- 
-// If transaction data is available in the URL 
-if(!empty($_GET['item_number']) && !empty($_GET['tx']) && !empty($_GET['amt']) && !empty($_GET['cc']) && !empty($_GET['st'])){ 
-    // Get transaction information from URL 
-    $item_number = $_GET['item_number'];  
-    $txn_id = $_GET['tx']; 
-    $payment_gross = $_GET['amt']; 
-    $currency_code = $_GET['cc']; 
-    $payment_status = $_GET['st']; 
-     
-    // Get product info from the database 
-    $productResult = $db->query("SELECT * FROM products WHERE id = ".$item_number); 
-    $productRow = $productResult->fetch_assoc(); 
-     
-    // Check if transaction data exists with the same TXN ID. 
-    $prevPaymentResult = $db->query("SELECT * FROM payments WHERE txn_id = '".$txn_id."'"); 
- 
-    if($prevPaymentResult->num_rows > 0){ 
-        $paymentRow = $prevPaymentResult->fetch_assoc(); 
-        $payment_id = $paymentRow['payment_id']; 
-        $payment_gross = $paymentRow['payment_gross']; 
-        $payment_status = $paymentRow['payment_status']; 
-    }else{ 
-        // Insert tansaction data into the database 
-        $insert = $db->query("INSERT INTO payments(item_number,txn_id,payment_gross,currency_code,payment_status) VALUES('".$item_number."','".$txn_id."','".$payment_gross."','".$currency_code."','".$payment_status."')"); 
-        $payment_id = $db->insert_id; 
-    } 
-} 
-?>
+include_once 'config.php';
 
+// Include database connection file 
+include_once 'dbConnect.php';
+
+// i need to get the product id from the cart_products table
+// Get the cart ID from the session
+$cart_id = $_SESSION['cart_id'];
+
+// Query the cart_products table to get the product ID
+$query = "SELECT products_id FROM cart_products WHERE cart_id = $cart_id";
+
+$mysqli = new mysqli("localhost", "root", "", "webshop");
+if ($mysqli->connect_errno) {
+    die("Verbindung fehlgeschlagen: " . $mysqli->connect_error);
+}
+
+$statement = $mysqli->prepare($query);
+$statement->execute();
+$result = $statement->get_result();
+while ($row = $result->fetch_assoc()) {
+    $product_id = $row['products_id'];
+
+
+    $sql = "INSERT INTO orders_products ( orders_products_id, orders_id ,products_id ) VALUES (?,?,?)";
+    $statement = $mysqli->prepare($sql);
+    $statement->bind_param("iii", $orders_products_id, $orders_id, $products_id);
+}
+
+
+
+
+
+
+?>
 <div class="container">
     <div class="status">
-        <?php if(!empty($payment_id)){ ?>
+        <?php if (!empty($payment_id)) { ?>
             <h1 class="success">Your Payment has been Successful</h1>
-			
+
             <h4>Payment Information</h4>
-            <p><b>Reference Number:</b> <?php echo $payment_id; ?></p>
-            <p><b>Transaction ID:</b> <?php echo $txn_id; ?></p>
-            <p><b>Paid Amount:</b> <?php echo $payment_gross; ?></p>
-            <p><b>Payment Status:</b> <?php echo $payment_status; ?></p>
-			
+            <p><b>Reference Number:</b>
+                <?php echo $payment_id; ?>
+            </p>
+            <p><b>Transaction ID:</b>
+                <?php echo $txn_id; ?>
+            </p>
+            <p><b>Paid Amount:</b>
+                <?php echo $payment_gross; ?>
+            </p>
+            <p><b>Payment Status:</b>
+                <?php echo $payment_status; ?>
+            </p>
+
             <h4>Product Information</h4>
-            <p><b>Name:</b> <?php echo $productRow['name']; ?></p>
-            <p><b>Price:</b> <?php echo $productRow['price']; ?></p>
-        <?php }else{ ?>
+            <p><b>Name:</b>
+                <?php echo $productRow['name']; ?>
+            </p>
+            <p><b>Price:</b>
+                <?php echo $productRow['price']; ?>
+            </p>
+        <?php } else { ?>
             <h1 class="error">Your Payment has Failed</h1>
         <?php } ?>
     </div>
-    <a href="../scripts/paymentSuccess.php" class="btn-link">Back to Products</a>
+    <a href="../products.php" class="btn-link">Back to Products</a>
 </div>
