de728a7ba8ffdf5d2fe1f14442c494026f0e7f53
JonasTisch <jonastisch@t-online.de>
2023-06-02 11:40:21 +0200

    erster search test

--- _public/php/search.php
+++ _public/php/search.php
@@ -1,60 +1,64 @@
 <?php include_once './header.php' ?>
 
+
+
+<!-- suchformular.php -->
+<form method="GET" action="searchresults.php">
+  <input type="text" name="searchterm" placeholder="Suche...">
+  <button type="submit">Suchen</button>
+</form>
+
+
+
 <?php
+
+
+if (isset($_GET['searchterm'])) {
+    $searchterm = $_GET['searchterm'];
+}
+
 
 $mysqli = new mysqli("localhost", "root", "", "webshop");
 	if ($mysqli->connect_errno) {
 		die("Verbindung fehlgeschlagen: " . $mysqli->connect_error);
 	}
-	$sql = "SELECT * FROM user WHERE username=? AND PASSWORD=?";
+
+
+
+	$sql = "SELECT * FROM product WHERE name LIKE %?%";
 	$statement = $mysqli->prepare($sql);
-	$statement->bind_param("ss", $username, $password);
+	$statement->bind_param("s", $searchterm);
 
 	$statement->execute();
 	$result = $statement->get_result();
 
-//$products = array(
-//    array("id" => 1, "name" => "Product 1", "description" => "Description of Product 1"),
-//    array("id" => 2, "name" => "Product 2", "description" => "Description of Product 2"),
-//    array("id" => 3, "name" => "Product 3", "description" => "Description of Product 3"),
-//    array("id" => 4, "name" => "Product 4", "description" => "Description of Product 4")
-//);
+    while ($row = $result->fetch_assoc()) {
+        echo "<div class='card shadow p-3 mb-5 bg-white rounded' >
+                <div class='card-body'>
+                <img src='" . $row['images'] . "' class='card-img-top img-fluid' style='max-height: 20vh; object-fit:contain;'>
+                  <h5 class='card-title'>" . $row['name'] . "</h5>
+                  
+                  
+                  <p class='card-text '>" . $row['description'] . "</p>
+                    <p class='card-text'>" . $row['price'] . "€</p>";
+        if ($GLOBALS['loggedIn']) {
+          echo "<form method='POST' action='./scripts/addToCartScript.php'>
+                  <input type='submit' name='add-to-cart' value='Zum Warenkorb hinzufügen' class='btn btn-primary'>
+                  <input type='hidden' name='products_id' value='" . $row['products_id'] . "'>
+                </form> </div>
+                </div>";
+        } else
+        //TODO: Link zu Login Seite MIT ID des Produkts oder wieder zurück zur Seite mit dem Produkt  
+        echo
+           "<a href='./login.php' class='btn btn-primary'>Zum Warenkorb hinzufügen (Login)</a>
 
-$searchTerm = isset($_GET['searchTerm']) ? $_GET['searchTerm'] : '';
+                </div>
+              </div>";
 
-if (!empty($searchTerm)) {
-    $filteredProducts = array_filter($products, function ($product) use ($searchTerm) {
-        return stripos($product['name'], $searchTerm) !== false || stripos($product['description'], $searchTerm) !== false;
-    });
-} else {
-    $filteredProducts = $products;
-}
-?>
-  <div class="container">
-        <h1>Webshop Search</h1>
+      }
 
-        <form action="" method="GET">
-            <div class="search-container">
-                <input type="text" name="searchTerm" placeholder="Search..." value="<?php echo $searchTerm; ?>">
-                <button type="submit" class="btn btn-primary">Search</button>
-            </div>
-        </form>
 
-        <div id="search-results">
-            <?php if (empty($filteredProducts)) : ?>
-                <p>No results found.</p>
-            <?php else : ?>
-                <ul class="list-group">
-                    <?php foreach ($filteredProducts as $product) : ?>
-                        <li class="list-group-item">
-                            <h5><?php echo $product['name']; ?></h5>
-                            <p><?php echo $product['description']; ?></p>
-                        </li>
-                    <?php endforeach; ?>
-                </ul>
-            <?php endif; ?>
-        </div>
-    </div>
+
 
     <?php include_once '../html/footer.html' ?>
 
