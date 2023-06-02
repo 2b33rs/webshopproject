99e397871501908727054818023b2ef89e119d45
phirec <phirec@outlook.de>
2023-06-02 12:49:00 +0200

    edit cart

--- _public/php/cart.php
+++ _public/php/cart.php
@@ -1,10 +1,10 @@
 
 <?php
+session_start();
 if($_SESSION['inactive'] = true){
     session_destroy();
     header("Location: login.php");
 }
-
 ?>
 
 
--- _public/php/scripts/autoLogoutScript.php
+++ _public/php/scripts/autoLogoutScript.php
@@ -6,7 +6,7 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
     // Überprüfen, ob die letzte Aktivität gespeichert ist
     if (isset($_SESSION['last_activity'])) {
         // Überprüfen, ob die maximale Inaktivitätsdauer überschritten wurde
-        $inactive_time = 5; // 3 Minuten (in Sekunden)
+        $inactive_time = 10; // 3 Minuten (in Sekunden)
         $current_time = time();
         $elapsed_time = $current_time - $_SESSION['last_activity'];
         $_SESSION['inactive'] = false;
