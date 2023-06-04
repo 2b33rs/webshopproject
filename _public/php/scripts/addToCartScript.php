d26466896a67d59a9471fb6e1c41a5521997d05b
Tobias <84900385+2b33rs@users.noreply.github.com>
2023-06-04 21:08:33 +0200

    6 Sekunden auf 600 (10 min)

--- _public/php/scripts/addToCartScript.php
+++ _public/php/scripts/addToCartScript.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (isset($_SESSION['timestamp']) && (time() - $_SESSION['timestamp'] > 6)) {
+if (isset($_SESSION['timestamp']) && (time() - $_SESSION['timestamp'] > 600)) {
     session_unset();
     session_destroy();
 }
