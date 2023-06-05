8c39079d393050962c3d10323c5d965825ffc8d6
phirec <phirec@outlook.de>
2023-06-05 10:42:52 +0200

    search edit

--- _public/php/header.php
+++ _public/php/header.php
@@ -40,7 +40,7 @@ include_once '../html/head.html';
                         ?>
                     </ul>
                     <ul id="nav_2" class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link" href="search.php"><i class="bi bi-search"></i>
+                        <li class="nav-item"><a class="nav-link" href="searchScript.php"><i class="bi bi-search"></i>
                                 Suche</a> </li>
 
                         <?php
--- _public/php/searchScript.php
+++ _public/php/searchScript.php
@@ -1,5 +1,5 @@
 <?php include_once './header.php' ?>
-
+<?php include_once '../html/search.html' ?>
 <?php
 
 $searchterm = "";
