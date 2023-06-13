480366dd3ce20f74a5481091edc5fbdecd91c9ff
phirec <phirec@outlook.de>
2023-06-13 22:31:49 +0200

    add address and email can now be changed

--- _public/php/scripts/userEditScript.php
+++ _public/php/scripts/userEditScript.php
@@ -1,6 +1,7 @@
 <?php
 
 if (isset($_POST["submit"])) {
+    
     $firstname = $_POST["firstname"];
     $name = $_POST["name"];
     $address = $_POST["address"];
@@ -12,9 +13,9 @@ if ($mysqli->connect_error) {
     die("Verbindung fehlgeschlagen: " . $mysqli->connect_error);
 }
 session_start();
-$sql = "UPDATE user SET name = ?, firstname = ? WHERE user_id = ?";
+$sql = "UPDATE user SET name = ?, firstname = ?, address=?, email=?  WHERE user_id = ?";
 $statement = $mysqli->prepare($sql);
-$statement->bind_param("ssi", $name, $firstname, $_SESSION["user_id"]);
+$statement->bind_param("ssssi", $name, $firstname, $address, $email, $_SESSION["user_id"]);
 $statement->execute();
 $statement->close();
 $mysqli->close();
