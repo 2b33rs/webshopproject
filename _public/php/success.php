c2750115d095ebb7ad40f1e8ab37206617c46ddd
phirec <phirec@outlook.de>
2023-06-14 15:06:28 +0200

    prepare for sql-injection

--- _public/php/success.php
+++ _public/php/success.php
@@ -5,57 +5,51 @@ include_once 'header.php';
 
 
 $mysqli = new mysqli("localhost", "root", "", "webshop");
-if ($mysqli->connect_errno) {
+if ($mysqli->connect_error) {
     die("Verbindung fehlgeschlagen: " . $mysqli->connect_error);
 }
 
-$sql = "SELECT cart_id FROM cart WHERE user_id = '" . $_SESSION["user_id"] . "'";
-$result = $mysqli->query($sql);
+$sql = "SELECT cart_id FROM cart WHERE user_id = ?"; //'" . $_SESSION["user_id"] . "'
+$statement = $mysqli->prepare($sql);
+$statement->bind_param("i", $_SESSION["user_id"]);
+$statement->execute();
+$result = $statement->get_result();
 $row = $result->fetch_assoc();
 if (!$result->num_rows == 0) {
     $cart_id = $row["cart_id"];
-    $sql = "SELECT products_id FROM cart_products WHERE cart_id = '" . $cart_id . "'";
-
-    $result = $mysqli->query($sql);
+    $sql = "SELECT products_id FROM cart_products WHERE cart_id = ?"; //'" . $cart_id . "'
+    $statement = $mysqli->prepare($sql);
+    $statement->bind_param("i", $cart_id);
+    $statement->execute();
+    $result = $statement->get_result();
     $date = date("Y-m-d");
 
     while ($row = $result->fetch_assoc()) {
 
-        $sql = "SELECT * FROM products WHERE products_id = '" . $row["products_id"] . "'";
-        $resultPro = $mysqli->query($sql);
-
+        $sql = "SELECT * FROM products WHERE products_id = ?"; //'" . $row["products_id"] . "'
+        $statement = $mysqli->prepare($sql);
+        $statement->bind_param("i", $row["products_id"]);
+        $statement->execute();
+        $resultPro = $statement->get_result();
         $rowPro = $resultPro->fetch_assoc();
 
         $sql = "INSERT INTO orders (user_id ,username ,name ,description ,price , purchase_date) VALUES (?,?,?,?,?,?)";
         $statement = $mysqli->prepare($sql);
-
         $statement->bind_param("isssds", $_SESSION["user_id"], $_SESSION["username"], $rowPro["name"], $rowPro["description"], $rowPro["price"], $date);
         $statement->execute();
     }
 
-    $sql = "DELETE FROM cart_products WHERE cart_id = '" . $cart_id . "'";
-    $mysqli->query($sql);
-    $sql = "DELETE FROM cart WHERE cart_id = '" . $cart_id . "'";
-    $mysqli->query($sql);
-
-
+    $sql = "DELETE FROM cart_products WHERE cart_id = ?"; //'" . $cart_id . "'
+    $statement = $mysqli->prepare($sql);
+    $statement->bind_param("i", $cart_id);
+    $statement->execute();
+    $sql = "DELETE FROM cart WHERE cart_id = ?"; //'" . $cart_id . "'
+    $statement = $mysqli->prepare($sql);
+    $statement->bind_param("i", $cart_id);
+    $statement->execute();
+    
 }
 
-
 ?>
-<main>
-    <div class="container">
-        <div class="card shadow p-xl-2 bg-white rounded ">
-            <div class="status card-body">
-                <h1 class="card-title">Your Payment has been Successful</h1><br>
-                <h3 class="card-text">Vielen Dank für Ihre Bestellung</h3>
-                <div class="mt-xl-5">
-                <a href="php/products.php" class="btn btn-primary float-start" role="button">zu den Produkten</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</main>
-
-
+<?php include_once '../html/success.html' ?>
 <?php include_once '../html/footer.html' ?>
