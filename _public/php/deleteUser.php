cce46492f1772558f5bd7a5e9f283a8b044c6a6c
phirec <phirec@outlook.de>
2023-06-14 15:12:03 +0200

    add delete useraccount

---
+++ _public/php/deleteUser.php
@@ -0,0 +1,11 @@
+<?php include_once 'header.php';
+
+// Check if user is still logged in
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+} ?>
+
+<?php include_once '../html/deleteUser.html' ?>
+
+
+<?php include_once '../html/footer.html' ?>
