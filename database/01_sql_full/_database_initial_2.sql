0ca0da3636e74f85e4212da21734d57ff49f8dfe
phirec <phirec@outlook.de>
2023-06-19 12:02:31 +0200

    fixed inti2

--- database/01_sql_full/_database_initial_2.sql
+++ database/01_sql_full/_database_initial_2.sql
@@ -56,7 +56,7 @@ DROP TABLE IF EXISTS `webshop`.`orders`;
 CREATE TABLE IF NOT EXISTS `webshop`.`orders` (
   `orders_id` INT NOT NULL AUTO_INCREMENT,
   `invoice_id` BIGINT NOT NULL,
-  `user_id` INT NOT NULL,
+  `user_id` INT,
   `username` VARCHAR(45) NOT NULL,
   `products_id` INT NOT NULL,
   `name` VARCHAR(255) NOT NULL,
